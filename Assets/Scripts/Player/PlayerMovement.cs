b75806f6b8be5361088e8a0f1286d4f07bb4f206
Jorge <jorgesakaino@gmail.com>
2022-09-25 02:25:06 -0500

    Cuy animations, textures and models & Dance

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,29 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float maxSpeed;
-
+    public Animator anim;
+    private Vector2 playerInput;
     public Rigidbody rgbd;
+    public float rotationSpeed;
 
     void Start()
     {
         rgbd = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Move();
+        PlayerRotation();
+        
     }
 
     public void Move()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        rgbd.velocity = new Vector3(horizontal * speed * Time.fixedDeltaTime, rgbd.velocity.y, vertical * speed * Time.fixedDeltaTime);
+        rgbd.velocity = new Vector3(playerInput.x * speed * Time.fixedDeltaTime, rgbd.velocity.y, playerInput.y * speed * Time.fixedDeltaTime);
 
         if (rgbd.velocity.magnitude > maxSpeed)
         {
             rgbd.velocity = Vector3.ClampMagnitude(rgbd.velocity, maxSpeed);
         }
+
+        if (rgbd.velocity.x != 0 || rgbd.velocity.z != 0)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+    private void PlayerRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = Camera.main.transform.forward * playerInput.y;
+        targetDirection += Camera.main.transform.right * playerInput.x;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero) targetDirection = transform.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+
     }
 }
